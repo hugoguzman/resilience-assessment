695c44c19b66297def757b2ebfca80d2859c5d40
Hugo <hugoguzman11@gmail.com>
2021-12-28 14:14:20 -0500

    more merges

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Routes, Route } from "react-router-dom";
 import TopAppBar from './Components/AppBar';
-import Home from './Home';
+import Home from './Routes/Home';
 import HealthandWellbeing from './Routes/HealthandWellbeing';
 import EconomyandSociety from './Routes/EconomyandSociety';
 import InfrastructureandEnvironment from './Routes/InfrastructureandEnvironment';
