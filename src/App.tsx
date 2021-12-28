bf8f0a28d15122e1ae5d177315dcb5fde178feb5
Hugo <hugoguzman11@gmail.com>
2021-12-28 14:10:18 -0500

    more merges

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import './App.css';
+import { Routes, Route } from "react-router-dom";
+import TopAppBar from './Components/AppBar';
+import Home from './Home';
+import HealthandWellbeing from './Routes/HealthandWellbeing';
+import EconomyandSociety from './Routes/EconomyandSociety';
+import InfrastructureandEnvironment from './Routes/InfrastructureandEnvironment';
+import LeadershipandStrategy from './Routes/LeadershipandStrategy';
 import { Layout } from './Components/Layout';
 import { LayoutGrid } from './Components/LayoutGrid';
 
 
-
 function App() {
   return (
-    <div>
+    <div className="App">
+      <TopAppBar />
       <Layout props>
         <LayoutGrid />
       </Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="health-and-wellbeing" element={<HealthandWellbeing />} />
+        <Route path="economy-and-society" element={<EconomyandSociety />} />
+        <Route path="infrastructure-and-environment" element={<InfrastructureandEnvironment />} />
+        <Route path="leadership-and-strategy" element={<LeadershipandStrategy />} />
+      </Routes>
     </div>
   );
 }
