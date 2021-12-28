fe7d193764aa4ac0fd401698a97302ac7fb95d5e
Alexander538 <88948463+Alexander538@users.noreply.github.com>
2021-12-28 15:13:02 -0500

    merged

--- src/Routes/HealthandWellbeing.tsx
+++ src/Routes/HealthandWellbeing.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import '../App.css';
-import { Layout } from '../Components/Layout';
 import { LayoutGrid } from '../Components/LayoutGrid';
 
 export default function HealthandWellbeing() {
     return (
-    <Layout props>
-      <LayoutGrid />
-    </Layout>
+    <LayoutGrid />
     );
   }
