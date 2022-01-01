1c9304b3aab5373f8b46b3161e0f00689f77770b
Alexander538 <88948463+Alexander538@users.noreply.github.com>
2022-01-01 15:01:13 -0500

    added formTab validation +

--- src/Components/FormTab.tsx
+++ src/Components/FormTab.tsx
@@ -4,9 +4,10 @@ import Tabs from '@mui/material/Tabs';
 import * as React from 'react';
 import { ConstantsCard } from './ConstantsCard';
 import { ParameterCard } from './ParameterCard';
-import { useForm } from 'react-hook-form';
+import { useForm, SubmitHandler } from 'react-hook-form';
 // import { DevTool } from "@hookform/devtools";
-import { InputProps, TextField, Button } from '@mui/material';
+import { InputProps, TextField, Button, IconButton } from '@mui/material';
+import HelpIcon from '@mui/icons-material/Help';
 
 type FormValues = {
 	name: string;
@@ -14,8 +15,8 @@ type FormValues = {
 	lastName: string;
 	// useFormRegisterReturn: any;
 	// register: (values: any) => void;
-	inputRef?: React.Ref<any>;
-	register: React.Ref<any>;
+	// inputRef?: React.Ref<any>;
+	// register: React.Ref<any>;
 };
 
 export const FormTab = () => {
@@ -25,11 +26,17 @@ export const FormTab = () => {
 		setSelectedTab(newValue);
 	};
 
-	const { register, control, formState } = useForm<FormValues>({
+	const { register, control, formState, handleSubmit, formState: { errors }, } = useForm<FormValues>({
 		mode: 'onBlur',
 		reValidateMode: 'onChange',
 		shouldUnregister: false,
 	});
+
+    console.log('errors', errors);
+
+    const formSubmitHandler: SubmitHandler<FormValues> = (data: FormValues) => {
+        console.log('form data is', data);
+    };
 
 	console.log(`form isDirty=${formState.isDirty}`);
 	console.log(formState.dirtyFields);
@@ -39,7 +46,7 @@ export const FormTab = () => {
 
 	return (
 		<div>
-			<form>
+			<form onSubmit={handleSubmit(formSubmitHandler)}>
 				{/* <DevTool control={control} /> */}
 				<Tabs value={selectedTab} onChange={handleTabChange}>
 					<Tab label='Name' />
@@ -48,22 +55,31 @@ export const FormTab = () => {
 				</Tabs>
 				{selectedTab === 0 && (
 					<>
+                    <div style={{ display: 'flex', alignItems: 'center', flex: 1, padding: 16, justifyContent: 'space-evenly'}}>
+                        <IconButton><HelpIcon></HelpIcon></IconButton>
+                        <p style={{display: 'block', width: '100%'}}>this is where the input prompt or question will go.</p>
 						<TextField
 							id='firstName'
 							//   name="firstName"
 							label='First Name'
 							type='text'
-							{...register('firstName')}
+							{...register('firstName', { required: true })}
 							//   inputRef={...register('name', { required: true })}
 						/>
+                        {errors.firstName && <span>This field is required</span>}
+                        </div>
+                        <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', flex: 1, padding: 16}}>
 						<TextField
 							id='lastName'
 							//   name="lastName"
 							label='Last Name'
 							type='text'
-							{...register('lastName')}
+							{...register('lastName', { required: true })}
 							//   inputRef={register()}
 						/>
+                        {errors.lastName && <span>This field is required</span>}
+                        <input type='submit'/>
+                        </div>
 					</>
 				)}
 				{selectedTab === 1 && (
@@ -76,13 +92,14 @@ export const FormTab = () => {
 					/>
 				)}
 				{selectedTab === 2 && (
-					<TextField
-						id='someField'
-						name='someField'
-						label='some field'
-						type='text'
-						// inputRef={register()}
-					/>
+					// <TextField
+					// 	id='someField'
+					// 	name='someField'
+					// 	label='some field'
+					// 	type='text'
+					// 	// inputRef={register()}
+					// />
+                    <ConstantsCard />
 				)}
 				{/* <Button>Submit</Button> */}
 			</form>
