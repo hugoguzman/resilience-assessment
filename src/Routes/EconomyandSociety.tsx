0578fa318816a4afeecbba4225fee75a724f579f
Alexander538 <88948463+Alexander538@users.noreply.github.com>
2022-01-01 21:07:28 -0500

    consolidated formTab component into health route comp

--- src/Routes/EconomyandSociety.tsx
+++ src/Routes/EconomyandSociety.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useState } from 'react';
 import '../App.css';
-import { FormTab } from '../Components/FormTab';
 
 
 export default function EconomyandSociety() {
